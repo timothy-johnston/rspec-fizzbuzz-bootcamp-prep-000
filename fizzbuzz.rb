8ed77d1a181488111f7b2466264e62ed1b23ee38
tj11 Johnston <tedwardj11@gmail.com>
2018-04-12 13:32:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
-#require 'pry'
+
 
 def fizzbuzz(int)
-#  binding.pry
   
-  if int % 3 == 0 
+  if int % 3 == 0 && int % 5 != 0
     puts "Fizz"
-  elseif int % 5 == 0 
+  elseif int % 5 == 0 && int % 3 != 0
     puts "Buzz"
   elseif int % 3 == 0 && int % 5 == 0 
     puts "FizzBuzz"
