4fa6d5904a27619b80ee8624bef6a504aec049de
tj11 Johnston <tedwardj11@gmail.com>
2018-04-12 13:36:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@ def fizzbuzz(int)
   
   if int % 3 == 0 && int % 5 != 0
     puts "Fizz"
-  elseif int % 5 == 0 && int % 3 != 0
+  elsif int % 5 == 0 && int % 3 != 0
     puts "Buzz"
-  elseif int % 3 == 0 && int % 5 == 0 
+  elsif int % 3 == 0 && int % 5 == 0 
     puts "FizzBuzz"
   end
   
