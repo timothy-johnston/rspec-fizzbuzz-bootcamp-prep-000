4afc3af89e9cb61efc5dff0824c0fb0fdcbdc9ed
tj11 Johnston <tedwardj11@gmail.com>
2018-04-12 13:37:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(int)
   
   if int % 3 == 0 && int % 5 != 0
-    puts "Fizz"
+    return "Fizz"
   elsif int % 5 == 0 && int % 3 != 0
     puts "Buzz"
   elsif int % 3 == 0 && int % 5 == 0 
